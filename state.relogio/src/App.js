7a9862e165db2f2a030f02d8526a7f0a3cfa706c
Malu Zarov Goncalves <90872311+MaluZarov@users.noreply.github.com>
2022-06-11 14:59:58 -0300

    relogio.jsx

---
+++ state.relogio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Relogio from './components/Relogio'
+
+function App() {
+  return (
+    <div className="App">
+     <Relogio />
+    </div>
+  );
+}
+
+export default App;
